893a6ffff09d536e53106e456408ac43f5582128
Bhaskar22Kamna <112804097+Bhaskar22Kamna@users.noreply.github.com>
2024-08-22 14:13:11 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 def welcome(name):
-    message = (f"hello {name} and welcom to world of game (WoG).\nhear you can find many coll gamse to play.")
+    message = (f"hello {name} and welcome to world of game (WoG).\nhear you can find many games to play.")
     return (message)
 
 
